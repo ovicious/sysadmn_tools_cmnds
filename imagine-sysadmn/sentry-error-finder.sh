471a23bb6af2841bffec1df86e7635f411a233ca
Avishek Barman <avishekbarman@gmail.com>
2020-01-14 18:31:31 +0600

    Imagine Radio ops Sentry Error Finder

---
+++ imagine-sysadmn/sentry-error-finder.sh
@@ -0,0 +1,3 @@
+http GET https://sentry.io/api/0/issues/904834989/events/\?project\=1400904\&statsPeriod\=30d "Authorization: Bearer 8b84fdd10a51400a9c1dc7af082ca87a7ee79a29bcb14f5e93eeda2af411965a" > sentry-event-oserror-2020-01-13.json
+
+
